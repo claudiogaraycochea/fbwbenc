c05a371daa684150be0de93543ae5347e7e22be4
claudiogaraycochea <computadoraweb@gmail.com>
2018-05-25 15:47:25 -0300

    Redux implementation

--- App.js
+++ App.js
@@ -1,29 +1,45 @@
-import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import React, { Component } from 'react';
+import { Provider, connect } from 'react-redux';
+import { addNavigationHelpers, StackNavigator } from "react-navigation";
 
-/* Navitation */
-import Welcome from './src/screens/WelcomeView';
-import Categories from './src/screens/CategoriesView';
-import CategoriesValidation from './src/screens/CategoriesValidationView';
-import SubCategories from './src/screens/SubCategoriesView';
-import HelpUs from './src/screens/HelpUsView';
-import Locate from './src/screens/LocateView';
-import LocallySelected from './src/screens/LocallySelectedView';
-import LocallyAnotherCity from './src/screens/LocallyAnotherCityView';
-import SearchesOpportunities from './src/screens/SearchesOpportunitiesView';
-import ThankYou from './src/screens/ThankYouView';
+import * as ReactNavigation from 'react-navigation'
 
-const App = createStackNavigator({
-  Welcome: { screen: Welcome },
-  Categories: { screen: Categories },
-  CategoriesValidation: { screen: CategoriesValidation },
-  SubCategories: { screen: SubCategories },
-  HelpUs: { screen: HelpUs },
-  Locate: { screen: Locate },
-  LocallySelected: { screen: LocallySelected },
-  LocallyAnotherCity: { screen: LocallyAnotherCity },
-  SearchesOportunities: { screen: SearchesOpportunities },
-  ThankYou: { screen: ThankYou }
-});
+import Routes from './src/config/routes'
 
-export default App;
+import getStore from "./src/config/store";
+
+const AppNavigator = StackNavigator(Routes);
+
+const navReducer = (state, action) => {
+  const newState = AppNavigator.router.getStateForAction(action, state);
+  return newState || state;
+};
+
+@connect(state => ({
+  nav: state.nav
+}))
+
+class AppWithNavigationState extends Component {
+  render() {
+      return (
+          <AppNavigator
+              navigation={addNavigationHelpers({
+                dispatch: this.props.dispatch,
+                state: this.props.nav
+              })}
+          />
+      );
+  }
+}
+
+const store = getStore(navReducer);
+
+export default class App extends Component {
+  render(){
+    return (
+      <Provider store={store}>
+        <AppWithNavigationState />
+      </Provider>
+    )
+  }
+}
